9f073dd15db808e6ddb3c430fa5175b86223d808
Eric Rochester <eric.rochester@willowtreeapps.com>
2018-12-02 19:39:44 -0500

    Day 1, part 2

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -1,15 +1,36 @@
 module Main where
 
-import Data.Maybe (mapMaybe)
 import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Data.ByteString.Lazy.Builder as B
+import Data.Foldable
+import qualified Data.HashSet as S
+import qualified Data.List as L
+import Data.Maybe
+import Data.Traversable
 
 main :: IO ()
 main = BS.interact
      $ B.toLazyByteString
      . (<> B.charUtf8 '\n')
      . B.intDec
-     . sum
+     . fromMaybe 0
+     . sumUntilDuplicate
+     . L.cycle
      . map fst
      . mapMaybe BS.readInt
      . BS.lines
+
+sumUntilDuplicate :: [Int] -> Maybe Int
+sumUntilDuplicate =
+    listToMaybe
+  . catMaybes
+  . map (\(_, _, x) -> x)
+  . L.scanl addFrequency (S.singleton (0 :: Int), 0, Nothing)
+
+addFrequency :: (S.HashSet Int, Int, Maybe Int) -> Int -> (S.HashSet Int, Int, Maybe Int)
+addFrequency (seen, accum, _) x
+  | accum' `S.member` seen = (seen, accum', Just accum')
+  | otherwise = (S.insert accum' seen, accum', Nothing)
+  where
+    accum' = x + accum
+
