e0fc9311e8329febd650401c9d26cfd33ea25161
Eric Rochester <eric.rochester@willowtreeapps.com>
2018-12-03 21:30:17 -0500

    Prep for day 4

---
+++ day4/app/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import qualified Advent
+import qualified Data.ByteString.Builder    as B
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+
+main :: IO ()
+main = Advent.interact partOne
+
+-- partOne
+
+partOne :: [BS.ByteString] -> B.Builder
+partOne = _
