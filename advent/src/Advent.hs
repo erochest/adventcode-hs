1a90888ea67381a37c0015eca24d3729d8d32424
Eric Rochester <eric.rochester@willowtreeapps.com>
2021-03-02 17:56:24 -0500

    adding stuff

--- advent/src/Advent.hs
+++ advent/src/Advent.hs
@@ -1,14 +1,27 @@
 module Advent
     ( interact
     , interactAll
-    ) where
+    , parse
+    )
+where
 
-import qualified Data.ByteString.Builder    as B
-import qualified Data.ByteString.Lazy.Char8 as BS
-import           Prelude                    hiding (interact)
+import qualified Data.ByteString.Builder       as B
+import qualified Data.ByteString.Lazy.Char8    as BS
+import           Prelude                 hiding ( interact )
+import           Data.Attoparsec.ByteString.Char8
+                                         hiding ( parse )
 
 interact :: ([BS.ByteString] -> B.Builder) -> IO ()
-interact f = BS.interact $ B.toLazyByteString . (<> B.charUtf8 '\n') . f . BS.lines
+interact f =
+    BS.interact
+        $ B.toLazyByteString
+        . (<> B.charUtf8 '\n')
+        . f
+        . filter (not . BS.null)
+        . BS.lines
 
 interactAll :: (BS.ByteString -> B.Builder) -> IO ()
 interactAll f = BS.interact $ B.toLazyByteString . (<> B.charUtf8 '\n') . f
+
+parse :: Parser a -> BS.ByteString -> Either String a
+parse p = parseOnly p . BS.toStrict
